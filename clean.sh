f7d1769f81002cd49145aca181bb5e7e87db2655
mnahinkhan <mnk1@andrew.cmu.edu>
2021-05-24 23:44:52 +0300

    Add comments to clean.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# For each .sh file in the current directory, remove the .md file with the
+# same name, suppressing any error outputs
+
 for f in *.sh
 do
     filename="${f%.*}"
     rm "$filename".md 2>/dev/null
 done
+
+# Remove index.md file, suppressing any error outputs
 
 rm index.md 2>/dev/null
 exit 0
