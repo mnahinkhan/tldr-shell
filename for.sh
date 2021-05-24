3aa114756ce9e3599fb321514444cf8b3f6f9589
mnahinkhan <mnk1@andrew.cmu.edu>
2021-05-24 21:16:16 +0300

    Add example shell scripts

---
+++ for.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Add "img-" in front of every png image filename
+for f in *.png
+do
+   mv f "img-$f.png" 
+done
---
+++ if.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Get IP info from the internet.
+# If it succeeds, print success message!
+# Otherwise, print that there are errors.
+if curl ipinfo.io; then
+    echo "Succeedede in getting your public IP details!"
+else
+    echo "Error getting your IP details..."
+fi
